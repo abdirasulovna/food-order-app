9e873179becd49b175b42815fabc80830af0ce78
Nuraiym <nabdirasulkyzy@gmail.com>
2022-11-01 15:35:25 +0500

    project

---
+++ src/components/Meals/MealsItem/MealItem.jsx
@@ -0,0 +1,13 @@
+import classes from "./MealItem.module.css";
+export function MealItem(props) {
+  const price = props.price.toFixed(2);
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <p className={classes.description}>{props.description}</p>
+        <p className={classes.price}>{price}</p>
+      </div>
+    </li>
+  );
+}
